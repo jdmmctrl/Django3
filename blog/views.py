7d96f6449d9b86b1aa3856d59684673b9848378d
jdmmctrl <juandavidmurillomejiaoutlook.es>
2023-06-29 17:57:09 -0500

    Actualizacion 29/06/2023/00

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import PostCreateForm
 
 
 class BlogListView(View):
@@ -8,5 +9,21 @@ class BlogListView(View):
 
         }
         return render(request, 'blog-list.html', context)
+
+
+class BlogCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = PostCreateForm()
+        context = {
+            'form': PostCreateForm()
+        }
+        return render(request, 'blog-create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = {
+            'form': PostCreateForm()
+        }
+        return render(request, 'blog-create.html', context)
+
 
 # Create your views here.
